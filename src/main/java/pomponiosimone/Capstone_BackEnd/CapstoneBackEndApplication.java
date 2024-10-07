f4a229eb5481175dbfc5ba16f4f532f21f462862
Simone Pomponio <Pomponio671@gmail.com>
2024-10-07 10:35:01 +0200

    Initial commit

---
+++ src/main/java/pomponiosimone/Capstone_BackEnd/CapstoneBackEndApplication.java
@@ -0,0 +1,13 @@
+package pomponiosimone.Capstone_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pomponiosimone/Capstone_BackEnd/CapstoneBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package pomponiosimone.Capstone_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
